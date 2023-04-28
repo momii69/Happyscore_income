52c1535ce8158bda6b7da75afa4738c641956ff5
momii69 <131962405+momii69@users.noreply.github.com>
2023-04-28 16:17:54 +0500

    Add files via upload

---
+++ Happyscore_income.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[4]:
+
+
+happy=pd.read_csv("C:\\Users\\My Pc\\Downloads\\happyscore_income.csv")
+
+
+# In[5]:
+
+
+happy
+
+
+# In[6]:
+
+
+happy.plot()
+plt.grid()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
